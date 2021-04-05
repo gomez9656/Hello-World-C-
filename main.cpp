4b34ff03692bb0b327bb69a276311fa48e440af7
Juan Gomez <juanpg9656@gmail.com>
2021-04-05 18:55:08 -0500

    variable declaration

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ using namespace std;
 int main()
 {
     cout << "Hola 1\n" << "Hello world!" << endl;
+
+    int var1;
+    char var2;
+    float var3;
+    double var4;
+    bool var5;
+
     return 0;
 }
