a4e4b5c457ca44d362e201deed005fbc8e4d2b5d
Juan Gomez <juanpg9656@gmail.com>
2021-04-05 19:06:06 -0500

    variable asign

--- main.cpp
+++ main.cpp
@@ -4,13 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hola 1\n" << "Hello world!" << endl;
+    cout << "Hola 1\n" << "Hello world!\n" << endl;
 
-    int var1;
+    int _var1;
     char var2;
     float var3;
     double var4;
     bool var5;
+
+    _var1 = 2;
+    var2 = 'c';
+    var3 = 3.4;
+    var4 = 6.7;
+    var5 = true;
+
+    cout << _var1 << endl;
+    cout << var2 << endl;
+    cout << var3 << endl;
+    cout << var4 << endl;
+    cout << var5 << endl;
 
     return 0;
 }
