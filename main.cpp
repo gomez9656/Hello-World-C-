409aba878981273b1dad86c7d2a7eca8e21f5cbe
Juan Gomez <juanpg9656@gmail.com>
2021-04-05 19:09:59 -0500

    cientific notation

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main()
     float var3;
     double var4;
     bool var5;
+    double var6 = 5.3e3;
+    double var7 = 7.7e-6;
 
     _var1 = 2;
     var2 = 'c';
@@ -18,11 +20,14 @@ int main()
     var4 = 6.7;
     var5 = true;
 
+
     cout << _var1 << endl;
     cout << var2 << endl;
     cout << var3 << endl;
     cout << var4 << endl;
     cout << var5 << endl;
+    cout << var6 << endl;
+    cout << var7 << endl;
 
     return 0;
 }
