e3c044f4ab2aaeb92f09773170a99822144c97e3
Juan Gomez <juanpg9656@gmail.com>
2021-04-05 19:15:42 -0500

    char with ascii

--- main.cpp
+++ main.cpp
@@ -8,18 +8,20 @@ int main()
 
     int _var1;
     char var2;
+    char letra;
     float var3;
     double var4;
     bool var5;
     double var6 = 5.3e3;
     double var7 = 7.7e-6;
 
+
     _var1 = 2;
     var2 = 'c';
     var3 = 3.4;
     var4 = 6.7;
     var5 = true;
-
+    letra = 65;
 
     cout << _var1 << endl;
     cout << var2 << endl;
@@ -28,6 +30,7 @@ int main()
     cout << var5 << endl;
     cout << var6 << endl;
     cout << var7 << endl;
+    cout << letra << endl;
 
     return 0;
 }
